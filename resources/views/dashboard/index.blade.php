fd12c4d0ca9ce00a9e963c06ac324d803c847fcc
Nanichang Katzing <nanipaul68@gmail.com>
2020-04-14 15:53:55 +0100

    m

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -32,6 +32,7 @@
         cursor: default;
       }
     </style>
+    <link href="{{ URL::asset('assets/css/offcanvas.css') }}" rel="stylesheet">
   </head>
   <body class="bg-light">
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
@@ -143,6 +144,7 @@
       <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
       <script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"><\/script>')</script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-      
-</body>
+      <script src="{{ URL::asset('assets/js/offcanvas.js') }}"></script>
+    </body>
+</body> 
 </html>
--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -62,5 +62,6 @@
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
 	<script type="text/javascript" src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/offcanvas.js') }}"></script>
 </body>
 </html>
